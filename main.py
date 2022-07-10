45e67bdd6cadc9c97ceb28f1d949a0fd4ed21039
martels <martel.s@husky.neu.edu>
2022-07-11 09:59:28 +1200

    Slightly improved scaling method

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ import Layer
 #Return Val: 
 
 
+"""
+!!this function is taken from examples on the internet!!
 #Function:      Round up   
 #Parameters:    number, decimals    
 #Return Val:    rounded number
+"""
 def round_up(n, decimals=0):
     multiplier = 10 ** decimals
     return math.ceil(n * multiplier) / multiplier
 
-
+"""
 #Function:      Open CSV file
 #Parameters:    csv file name
 #Return Val:    values for scale and colour
+"""
 def open_csv(file_name):
     output = []
     with open(file_name) as csv_file:
@@ -40,20 +44,22 @@ def open_csv(file_name):
                 line_count += 1
     return output
 
-
+"""
 #Function:      Get Scale values from list
 #Parameters:    Input list
 #Return Val:    values for scale
+"""
 def get_scale_values(parameters):
     output = []
     for x in range(len(parameters)):
         output.append(float(parameters[x][0]))
     return output
 
-
+"""
 #Function:      Get Colour values from list
 #Parameters:    Input list
 #Return Val:    values for colour
+"""
 def get_colour_values(parameters):
     output = []
     for x in range(len(parameters)):
@@ -61,16 +67,19 @@ def get_colour_values(parameters):
         output.append(temp)
     return output
 
-
+"""
 #Function:      Convert scale values to pixel values
 #Parameters:    Scale Values
 #Return Val:    initial pixel number
+"""
 def scale_image(values):
-    start_pixel = 1000
-    total = sum(values)
-    temp = start_pixel/total
-    output = math.ceil(round_up(temp)*total)
-    return output
+    unit_value = 100
+    scale_factor = 10
+    unit_scales = values * unit_value
+    scale_sum = sum(unit_scales)
+    total_image_size = round_up(scale_factor * scale_sum)
+    return int(total_image_size)
+
 ### !!!Not a good scaling method!!! ###
 
 
@@ -81,8 +90,10 @@ def main():
     #print(scale_values)
     colour_values = get_colour_values(seed_values)
     #print(colour_values)
-    image_size = scale_image(scale_values)
+    image_size = 5000   
+#    image_size = scale_image(scale_values)
     square_scale = int(image_size/2)
+    print(image_size, square_scale)
     image = Image.new("RGB", (image_size, image_size), "white")    
 
     layers = []
@@ -102,3 +113,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# Need Better naming  convensions
+# Need better function descriptions
+# Should make functions more specialized and not round with in build functions of Python 
